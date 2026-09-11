c1ae30ac51a8ae4bd823fc5ceb49841f75ea0a05
eriksundenius <erik.sundenius@gmail.com>
2026-09-11 09:18:19 +0200

    börja dashboard

--- login-risk-detector/Controllers/HomeController.cs
+++ login-risk-detector/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using System.Diagnostics;
+using System.Linq;
+using login_risk_detector.Data;
 using login_risk_detector.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,11 +9,34 @@ namespace login_risk_detector.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        //IActionResult innebär att returtypen är en webb vy
+        public async Task<IActionResult> Index()
+        {
+            //hämta 50 senaste loginevents
+            var recentEvents = await _context.LoginEvents
+                .OrderByDescending(equals => e.Timestamp)
+                .Take(50
+                .ToListAsync();
+
+            //riskscore per specifik användare
+            var userRisks = await _context.LoginEvents
+                .GroupBy(e => e.UserId)
+                .Select(g => g.OrderByDescending e => e.Timestampt).FirsOrDefault())
+
+
+        }
+
 
         public IActionResult Index()
         {
